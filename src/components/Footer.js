aaa1033d845d45ab83d750e74f4cbeb204bdf5cd
karim <k.guezlane@gmail.com>
2021-08-03 19:41:08 +0100

    Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+	return (
+		<div id="footer">
+			<p>
+				Copyright © Created by{" "}
+				<a href="https://github.com/krimoelbahri"> Elbahri </a>. TOP
+				project
+			</p>
+		</div>
+	);
+}
+export { Footer };
