5fb13f8d6f00867dcb1559cc650bbe64300d0084
karim <k.guezlane@gmail.com>
2021-08-03 19:38:42 +0100

    setting up the array with cards informations

---
+++ src/components/main-components/Cardsarray.js
@@ -0,0 +1,75 @@
+let cardsArray = [
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "Messi",
+	},
+];
+export { cardsArray };
