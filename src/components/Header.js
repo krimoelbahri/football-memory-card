4c521d24b4e4d4ccefa4c4df363699e591e3d5c1
karim <k.guezlane@gmail.com>
2021-08-03 19:40:51 +0100

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<div id="header">
+			<div id="websiteTitle">
+				<i className="fas fa-futbol pd-5px"></i>
+				<h1 className="pd-5px">Foot Memory Card</h1>
+			</div>
+		</div>
+	);
+}
+export { Header };
