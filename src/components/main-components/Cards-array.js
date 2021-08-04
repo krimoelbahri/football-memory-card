0985c911db2b9bd9a6115a4d76aaf603f4589e18
karim <k.guezlane@gmail.com>
2021-08-04 10:59:32 +0100

    a module with players informations

---
+++ src/components/main-components/Cards-array.js
@@ -0,0 +1,87 @@
+let cardsArray = [
+	{
+		imgSrc:
+			"https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg",
+		playerName: "Lionel Messi",
+		playerId: "LM10",
+		index: 0,
+	},
+	{
+		imgSrc:
+			"https://i.pinimg.com/736x/1e/fe/6e/1efe6ee1f8d80bb9486697c28928e485.jpg",
+		playerName: "Cristiano Ronaldo",
+		playerId: "CR7",
+		index: 1,
+	},
+	{
+		imgSrc:
+			"https://images.caradisiac.com/logos/0/8/0/2/140802/S0-Apres-la-Coupe-du-Monde-Ronaldo-risque-de-perdre-son-permis-51968.jpg",
+		playerName: "Nazario Ronaldo",
+		playerId: "R9",
+		index: 2,
+	},
+	{
+		imgSrc:
+			"https://i.pinimg.com/736x/b1/b3/b8/b1b3b8e1921d9c5906d750a5fc93d75c.jpg",
+		playerName: "Ronaldinho Gaúcho",
+		playerId: "RG10",
+		index: 3,
+	},
+	{
+		imgSrc:
+			"https://i.pinimg.com/474x/bb/f7/29/bbf729c0c40edb225834beff62678064.jpg",
+		playerName: "Marcelo da Silva",
+		playerId: "MD12",
+		index: 4,
+	},
+	{
+		imgSrc:
+			"https://www.thesun.co.uk/wp-content/uploads/2021/01/59b5de56-74ca-4635-b11c-8312f76ab93e.jpg",
+		playerName: "Willian da Silva",
+		playerId: "WB12",
+		index: 5,
+	},
+	{
+		imgSrc:
+			"https://i2.wp.com/i.dailymail.co.uk/1s/2021/02/27/22/39846128-9307777-image-m-38_1614463517551.jpg",
+		playerName: "Gerard Piqué",
+		playerId: "GP3",
+		index: 6,
+	},
+	{
+		imgSrc:
+			"https://i2-prod.dailystar.co.uk/incoming/article21244202.ece/ALTERNATES/s1200c/1_JS202853778.jpg",
+		playerName: "Kylian Mbappé",
+		playerId: "KM10",
+		index: 7,
+	},
+	{
+		imgSrc:
+			"https://idsb.tmgrup.com.tr/ly/uploads/images/2020/11/26/75152.jpg",
+		playerName: "Zlatan Ibrahimovich",
+		playerId: "ZI9",
+		index: 8,
+	},
+	{
+		imgSrc:
+			"https://i.pinimg.com/originals/fb/05/57/fb0557a657629e13c80dbbf67b8e2e53.jpg",
+		playerName: "Zinedine Zidane",
+		playerId: "Zizo10",
+		index: 9,
+	},
+	{
+		imgSrc:
+			"https://sportstar.thehindu.com/incoming/article34775301.ece/ALTERNATES/LANDSCAPE_1200/Neymarjpg",
+		playerName: "Neymar da Silva Júnior",
+		playerId: "NJ10",
+		index: 10,
+	},
+	{
+		imgSrc:
+			"https://static.toiimg.com/thumb/msid-79250859,imgsize-98293,width-400,resizemode-4/79250859.jpg",
+		playerName: "Riyad Mahrez",
+		playerId: "RM7",
+		index: 11,
+	},
+];
+export { cardsArray };
