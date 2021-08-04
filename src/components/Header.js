be52509cc4abcfb62e2ce4bfc0ca09e2171f13ec
karim <k.guezlane@gmail.com>
2021-08-04 10:56:49 +0100

    Changed the title and added animation for tha ball

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 function Header() {
 	return (
-		<div id="header">
-			<div id="websiteTitle">
-				<i className="fas fa-futbol pd-5px"></i>
-				<h1 className="pd-5px">Foot Memory Card</h1>
+		<div id='header'>
+			<div id='websiteTitle'>
+				<h1 className='pd-5px'>Foot</h1>
+				<i className='fas fa-futbol pd-5px bounce-1'></i>
+				<h1 className='pd-5px'>Memory Card</h1>
 			</div>
 		</div>
 	);
