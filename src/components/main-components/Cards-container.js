011fbf2bb3e578dfcca854308bd130e224e024ec
karim <k.guezlane@gmail.com>
2021-08-04 11:00:55 +0100

    addidng logic to handle card clicking

--- src/components/main-components/Cards-container.js
+++ src/components/main-components/Cards-container.js
@@ -1,13 +1,30 @@
-import React, { useState, useEffect } from "react";
 import { Card } from "./Card";
+import React, { useState, useEffect } from "react";
+import { cardsArray } from "./Cards-array";
+import { shuffleArray } from "../helpers/array-helpers";
 
 function CardsContainer(props) {
+	const [shuffledArray, setShuffledArray] = useState(cardsArray);
+	const handleClick = (e) => {
+		setShuffledArray(shuffleArray(cardsArray));
+		let index = e.target.dataset.index;
+		props.handleScore(cardsArray[index]);
+	};
+	useEffect(() => {
+		setShuffledArray(shuffleArray(cardsArray));
+		return () => {};
+	}, []);
 	return (
-		<div id="cards-container">
-			{props.cardsArray.map((card, i) => {
+		<div id='cards-container'>
+			{shuffledArray.map((card, i) => {
 				return (
 					<React.Fragment key={card.playerId + i}>
-						<Card imgSrc={card.imgSrc} playerName={card.playerName} />
+						<Card
+							handleClick={handleClick}
+							imgSrc={card.imgSrc}
+							playerName={card.playerName}
+							index={card.index}
+						/>
 					</React.Fragment>
 				);
 			})}
