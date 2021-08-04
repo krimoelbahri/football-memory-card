72330701fe87a491dbe23277cd002229e3232ef2
karim <k.guezlane@gmail.com>
2021-08-04 11:01:27 +0100

    fixing typos

--- src/components/main-components/Score-board.js
+++ src/components/main-components/Score-board.js
@@ -1,13 +1,13 @@
 function ScoreBoard(props) {
 	return (
-		<div className="score-board mrg-5px flex-r">
-			<div className="current-score mrg-5px flex-r">
-				<p className="pd-5px">Current score:</p>
-				<p className="pd-5px">{props.currentScore}</p>
+		<div className='score-board mrg-5px flex-r'>
+			<div className='current-score mrg-5px flex-r'>
+				<p className='pd-5px'>Current Score:</p>
+				<p className='pd-5px'>{props.currentScore}</p>
 			</div>
-			<div className="high-score mrg-5px flex-r">
-				<p className="pd-5px">high score:</p>
-				<p className="pd-5px">{props.highScore}</p>
+			<div className='high-score mrg-5px flex-r'>
+				<p className='pd-5px'>High Score:</p>
+				<p className='pd-5px'>{props.highScore}</p>
 			</div>
 		</div>
 	);
