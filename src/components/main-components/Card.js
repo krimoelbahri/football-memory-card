993fa7beb5cce23680abf3723cccb10d2d8657e4
karim <k.guezlane@gmail.com>
2021-08-04 10:59:00 +0100

    added data attributes to card elements

--- src/components/main-components/Card.js
+++ src/components/main-components/Card.js
@@ -1,11 +1,18 @@
 function Card(props) {
 	return (
-		<div className="card mrg-5px">
-			<div className="card-img-div">
-				<img src={props.imgSrc} alt={props.playerName} />
+		<div
+			onClick={props.handleClick}
+			className='card mrg-5px'
+			data-index={props.index}>
+			<div data-index={props.index} className='card-img-div'>
+				<img
+					src={props.imgSrc}
+					alt={props.playerName}
+					data-index={props.index}
+				/>
 			</div>
-			<div className="card-playerName pd-5px">
-				<h6>{props.playerName}</h6>
+			<div className='card-playerName pd-5px' data-index={props.index}>
+				<h6 data-index={props.index}>{props.playerName}</h6>
 			</div>
 		</div>
 	);
