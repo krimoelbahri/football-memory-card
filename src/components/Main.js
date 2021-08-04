156802c4b91cd822fee73b380dbac68b4bb51df5
karim <k.guezlane@gmail.com>
2021-08-04 10:57:39 +0100

    added game logic to the main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
 import { CardsContainer } from "./main-components/Cards-container";
-import { cardsArray } from "./main-components/Cardsarray";
 import { ScoreBoard } from "./main-components/Score-board";
 
 function Main() {
+	const [currentScore, setCurrentScore] = useState(0);
+	const [highScore, setHighScore] = useState(0);
+	const [gameArray, setGameArray] = useState([]);
+	const handleScore = (obj) => {
+		if (!gameArray.includes(obj)) {
+			setGameArray((prevArray) => prevArray.concat(obj));
+			setCurrentScore((currentScore) => currentScore + 1);
+			setHighScore((highScore) =>
+				currentScore < highScore ? highScore : currentScore + 1
+			);
+		} else {
+			setGameArray([]);
+			setCurrentScore(0);
+		}
+	};
+
 	return (
-		<div id="main">
-			<ScoreBoard currentScore="0" highScore="0" />
-			<CardsContainer cardsArray={cardsArray} />
+		<div id='main'>
+			<ScoreBoard currentScore={currentScore} highScore={highScore} />
+			<CardsContainer handleScore={handleScore} />
 		</div>
 	);
 }
